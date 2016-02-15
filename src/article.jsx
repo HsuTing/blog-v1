912da1fc943c52dcc799174da3aaddff990c19fd
HsuTing <hsuting0106@gmail.com>
2016-02-15 23:06:00 +0800

    add article

--- src/article.jsx
+++ src/article.jsx
@@ -5,7 +5,26 @@ import React from 'react';
 export default class Article extends React.Component {
   render() {
     return (
-      <div>test</div>
+      <div>
+        <h3 className="mdl-typography--display-2">{this.props.title}</h3>
+        <h6 className="mdl-typography--title mdl-color-text--grey-400">{this.props.date}</h6>
+        {this.props.article.map((d, i) => {
+          if(typeof(d) == 'object') {
+            let label = Object.keys(d)[0];
+            switch(label) {
+              default:
+                return null;
+            }
+          }
+
+          return (
+            <p key={i}
+               className="mdl-typography--body-1"
+               dangerouslySetInnerHTML={{__html: d}}
+            ></p>
+          );
+        })}
+      </div>
     );
   }
 };
--- src/index.jsx
+++ src/index.jsx
@@ -11,16 +11,28 @@ import List from './list';
 (() => {
   let id = location.search.replace('?no=', '');
 
-  GetJson('./data/index.json', (data) => {
+  GetJson('./data/list.json', (data_list) => {
     ReactDOM.render(<List style={"mdl-color-text--grey-600"} data={ListData} />, document.getElementById('list-header'));
     ReactDOM.render(<List data={ListData} />, document.getElementById('list-menu'));
 
-    if(data[id] != undefined) {
-      require.ensure([], function() {
-        let Article = require('./article').default; 
-        ReactDOM.render(<Article />, document.getElementById('main-part'));
+    if(data_list[id] != undefined) {
+      let file_path = './data/article/' + data_list[id].title.replace(/ /g, '-') + '.json';
+
+      GetJson(file_path, (data_article) => {
+        require.ensure([], () => {
+          let Article = require('./article').default; 
+          ReactDOM.render(
+            <Article title={data_list[id].title}
+                     date={data_list[id].date}
+                     article={data_article}
+            />, document.getElementById('main-part')
+          );
+        });
+      }, () => {
+        alert('You can not connect to server.');
       });
     }
+
   }, () => {
     alert('You can not connect to server.');
   });
